72849d6a2cab4225ab66ae5b628d2f41dcbd2a0e
hungkn32 <122142701+hungkn32@users.noreply.github.com>
2023-11-30 10:30:27 +0700

    hung

--- app/src/main/java/com/example/duan_1/Database/DBHelper.java
+++ app/src/main/java/com/example/duan_1/Database/DBHelper.java
@@ -8,7 +8,7 @@ import androidx.annotation.Nullable;
 
 public class DBHelper extends SQLiteOpenHelper {
     public static final String dbname = "QLSHOES";
-    public static final int dbvesion =17;
+    public static final int dbvesion =24;
 
     public DBHelper(Context context) {
 
@@ -40,10 +40,20 @@ public class DBHelper extends SQLiteOpenHelper {
                 "diaChi TEXT    REFERENCES KHACHHANG(diaChi) NOT NULL," +
                 "tenGiay TEXT    REFERENCES GIAY(tenGiay) NOT NULL," +
                 "loaiGiay TEXT    REFERENCES GIAY(loaiGiay) NOT NULL," +
-                "giaTien TEXT   REFERENCES GIAY(giaTien) NOT NULL," +
+                "giaTien TEXT   REFERENCES GIAY(giaTien) NoT NULL," +
                 "ngayMua DATE NOT NULL," +
                 "trangThai INTEGER NOT NULL)";
         db.execSQL(tb_dathang);
+        String donHang = "CREATE TABLE DONHANG(" +
+                " madonhang integer primary key autoincrement," +
+                " tenKH text REFERENCES KHACHHANG(tenKH)," +
+                " diaChi text REFERENCES KHACHHANG(diaChi)," +
+                " tenGiay text REFERENCES GIAY(tenGiay)," +
+                " loaiGiay text REFERENCES GIAY(loaiGiay)," +
+                " ngaydathang text not null," +
+                " tongtien integer not null," +
+                " trangthai text not null)";
+        db.execSQL(donHang);
 
         db.execSQL("Insert into ADMIN values('admin','admin','admin123')," +
                 "('nhanvien','nhanvien','nhanvien123')");
@@ -52,10 +62,13 @@ public class DBHelper extends SQLiteOpenHelper {
             "(2,'Giày Adidas','Adidas','29999','https://product.hstatic.net/200000255805/product/z3744531959920_d9759f17887e00d6899ff0650ba0b8ca_70e805bd5bba4a89b08f7cab50030603_master.jpg')," +
             "(3,'Giày Bitis','Bitis','39999','https://product.hstatic.net/1000230642/product/-hunter-x-wavy-collection-hsm001400xnh-xanh-nhot-5o654-color-xanh-nhot_1d01269da8544dc7956d1c0c363b2f6b_grande.jpg')");
         // Ví dụ về cách chèn dữ liệu mẫu vào bảng "GIAY"
-        db.execSQL("INSERT INTO KHACHHANG VALUES(1,'Hưng','2004','0347980858','Thôn 9 Hà Nội ','https://png.pngtree.com/png-vector/20190623/ourlarge/pngtree-accountavataruser--flat-color-icon--vector-icon-banner-templ-png-image_1491720.jpg')");
+        db.execSQL("INSERT INTO KHACHHANG VALUES(1,'Hưng','2004','0347980858','Thôn 9 Hà Nội ','https://png.pngtree.com/png-vector/20190623/ourlarge/pngtree-accountavataruser--flat-color-icon--vector-icon-banner-templ-png-image_1491720.jpg')," +
+                "(2,'Thành','2003','0347980858','Bắc Giang Lục Ngạn ','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThWjSq0pSKoXUjoRq6-xtbiXRX9w2qOm5N8EepZIAfRQ&s')," +
+                "(3,'Giang','2004','0347980858','Thôn 9 Hà Nội ','https://png.pngtree.com/png-vector/20190704/ourlarge/pngtree-businessman-user-avatar-free-vector-png-image_1538405.jpg')");
 
-
-
+db.execSQL("INSERT INTO DONHANG VALUES(1,'Hưng KN','Thôn 9 Hà Nội','Giày Nike White','Nike','30/11/2023',3999,'Đã Nhận Hàng')," +
+        "(2,'Thành KN','Thôn 9 Hà Nội','Giày Adidas','Adidas','30/11/2023',2999,'Đã Nhận Hàng')," +
+        "(3,'Giang KN','Thôn 9 Hà Nội','Giày Bitis','Bitis','30/11/2023',4999,'Đã Nhận Hàng')");
 
 
     }
@@ -67,6 +80,7 @@ public class DBHelper extends SQLiteOpenHelper {
             db.execSQL("DROP TABLE IF EXISTS GIAY");
             db.execSQL("DROP TABLE IF EXISTS KHACHHANG");
             db.execSQL("DROP TABLE IF EXISTS DATHANG");
+            db.execSQL("DROP TABLE IF EXISTS DONHANG");
             onCreate(db);
         }
 
